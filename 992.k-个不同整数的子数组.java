e7c6b53746c110891b147b262c0b089c06215ecb
MarineHero! <897225861@qq.com>
2021-02-09 17:53:36 +0800

    992

---
+++ 992.k-个不同整数的子数组.java
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode.cn id=992 lang=java
+ *
+ * [992] K 个不同整数的子数组
+ */
+
+// @lc code=start
+class Solution {
+    public int subarraysWithKDistinct(int[] A, int K) {
+        int n = A.length;
+        int[] num1 = new int[n + 1];
+        int[] num2 = new int[n + 1];
+        int tot1 = 0, tot2 = 0;
+        int left1 = 0, left2 = 0, right = 0;
+        int res = 0;
+        while (right < n) {
+            if (num1[A[right]] == 0) {
+                tot1++;
+            }
+            num1[A[right]]++;
+            if (num2[A[right]] == 0) {
+                tot2++;
+            }
+            num2[A[right]]++;
+            while (tot1 > K) {
+                num1[A[left1]]--;
+                if (num1[A[left1]] == 0) {
+                    tot1--;
+                }
+                left1++;
+            }
+            while (tot2 > K - 1) {
+                num2[A[left2]]--;
+                if (num2[A[left2]] == 0) {
+                    tot2--;
+                }
+                left2++;
+            }
+            res += left2 - left1;
+            right++;
+        }
+        return res;
+    }
+}
+// @lc code=end
+
