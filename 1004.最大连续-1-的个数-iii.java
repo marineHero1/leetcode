02194083d3bcbd1d413929b9e1f6d67d27aa35cb
yzy0073 <yzy0073@auburn.edu>
2021-02-19 15:05:10 +0800

    1004

---
+++ 1004.最大连续-1-的个数-iii.java
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=1004 lang=java
+ *
+ * [1004] 最大连续1的个数 III
+ */
+
+// @lc code=start
+class Solution {
+    public int longestOnes(int[] A, int K) {
+        int N = A.length;
+        int res = 0;
+        int left = 0, right = 0;
+        int zeros = 0;
+        while (right < N) {
+            if (A[right] == 0)
+                zeros ++;
+            while (zeros > K) {
+                if (A[left++] == 0) 
+                    zeros --;
+            }
+            res = Math.max(res, right - left + 1);
+            right ++;
+        }
+        return res;
+    }
+}
+
+// @lc code=end
+
