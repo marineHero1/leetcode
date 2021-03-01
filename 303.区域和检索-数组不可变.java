a88391dae13a6b96956e7ab35b5f2212b927a6fa
月不明 沧海 <897225861@qq.com>
2021-03-01 13:27:43 +0800

    303

---
+++ 303.区域和检索-数组不可变.java
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=303 lang=java
+ *
+ * [303] 区域和检索 - 数组不可变
+ */
+
+// @lc code=start
+class NumArray {
+    int[] sums;
+
+    public NumArray(int[] nums) {
+        int n = nums.length;
+        sums = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            sums[i + 1] = sums[i] + nums[i];
+        }
+    }
+    
+    public int sumRange(int i, int j) {
+        return sums[j + 1] - sums[i];
+    }
+}
+
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * int param_1 = obj.sumRange(i,j);
+ */
+// @lc code=end
+
